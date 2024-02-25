bef8ff770acb7c19c44223e17af1bf15a345b773
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-02-25 22:44:39 +0545

    campgrounds commit

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log('Database Connected!!');
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -10,10 +20,9 @@ app.get('/', (req, res)=> {
     res.render('home')
 })
 
-app.get('/makecampground', async (req,res) => {
-    const camp = new Campground({title: 'My BackYard', description : 'Cheap Camping!'});
-    camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req,res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds})
 })
 
 app.listen(3000, () => {
