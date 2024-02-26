f1e7f1ca7603383b827065a2cc3f6911c4712a3a
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-02-26 22:45:11 +0545

    styling yelpcamp

--- models/campground.js
+++ models/campground.js
@@ -1,18 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log('Database Connected!!');
-});
-
 const campgroundSchema = new Schema({
     title:String,
-    price: String,
+    image: String,
+    price: Number,
     description: String,
     location : String
 });
