e454cd54685dba8170cdae39ce59c736516fa398
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-03-09 12:13:15 +0545

    Configuring session

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const campgrounds =  require('./routes/campgrounds');
 const reviews =  require('./routes/reviews');
+const session = require('express-session');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -18,6 +19,18 @@ db.once("open", () => {
 
 const app = express();
 
+const sessionConfig = {
+    secret: 'thisshouldbebettersecret!',
+    resave: false,
+    saveuninitialized: true,
+    cookie: {
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+
+
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -25,6 +38,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sessionConfig));
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
