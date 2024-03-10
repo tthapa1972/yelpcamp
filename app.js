12b0f3c9cf38187d415f18b50166d2d4d805bd5d
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-03-10 11:39:43 +0545

    Configuring Passport

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const campgrounds =  require('./routes/campgrounds');
 const reviews =  require('./routes/reviews');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -41,6 +44,13 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
