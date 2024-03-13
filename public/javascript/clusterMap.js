5e12abe62db89e63dd5fe1e10d649d491c60a5f7
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-03-13 17:41:52 +0545

    Tweaking clustering Code

--- public/javascript/clusterMap.js
+++ public/javascript/clusterMap.js
@@ -1,4 +1,5 @@
 mapboxgl.accessToken = mapBoxToken;
+const campgroundsObj = JSON.parse(campgrounds);
     const map = new mapboxgl.Map({
         container: 'map',
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
@@ -11,11 +12,11 @@ mapboxgl.accessToken = mapBoxToken;
         // Add a new source from our GeoJSON data and
         // set the 'cluster' option to true. GL-JS will
         // add the point_count property to your source data.
-        map.addSource('earthquakes', {
+        map.addSource('campgrounds', {
             type: 'geojson',
             // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
             // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-            data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+            data: campgroundsObj,
             cluster: true,
             clusterMaxZoom: 14, // Max zoom to cluster points on
             clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
@@ -24,7 +25,7 @@ mapboxgl.accessToken = mapBoxToken;
         map.addLayer({
             id: 'clusters',
             type: 'circle',
-            source: 'earthquakes',
+            source: 'campgrounds',
             filter: ['has', 'point_count'],
             paint: {
                 // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
@@ -56,7 +57,7 @@ mapboxgl.accessToken = mapBoxToken;
         map.addLayer({
             id: 'cluster-count',
             type: 'symbol',
-            source: 'earthquakes',
+            source: 'campgrounds',
             filter: ['has', 'point_count'],
             layout: {
                 'text-field': ['get', 'point_count_abbreviated'],
@@ -68,7 +69,7 @@ mapboxgl.accessToken = mapBoxToken;
         map.addLayer({
             id: 'unclustered-point',
             type: 'circle',
-            source: 'earthquakes',
+            source: 'campgrounds',
             filter: ['!', ['has', 'point_count']],
             paint: {
                 'circle-color': '#11b4da',
@@ -84,7 +85,7 @@ mapboxgl.accessToken = mapBoxToken;
                 layers: ['clusters']
             });
             const clusterId = features[0].properties.cluster_id;
-            map.getSource('earthquakes').getClusterExpansionZoom(
+            map.getSource('campgrounds').getClusterExpansionZoom(
                 clusterId,
                 (err, zoom) => {
                     if (err) return;
