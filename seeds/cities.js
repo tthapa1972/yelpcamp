dbc15bfff196acc2edf533ef33113f71f544e407
Tara Bahadur Thapa <tarabahadur.thapa@gmail.com>
2024-03-15 19:49:28 +0545

    Reseeding campground and bug fixes

--- seeds/cities.js
+++ seeds/cities.js
@@ -9,7 +9,7 @@ module.exports = [
         state: "New York",
     },
     {
-        city: "Los Angeles",
+        city: "Los  Angeles",
         growth_from_2000_to_2013: "4.8%",
         latitude: 34.0522342,
         longitude: -118.2436849,
